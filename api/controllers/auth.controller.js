931c73e72651afeae79ba4ef9f5ef6c5d27f7776
Bikal69 <143072556+Bikal69@users.noreply.github.com>
2024-02-05 13:36:15 +0545

    add functionality to the signup page

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -3,6 +3,7 @@ import bcryptjs from 'bcryptjs'
 import { errorHandler } from "../utils/error.js";
 export const signup=async (req,res,next)=>{
     const {username,email,password}=req.body;
+    console.log(req.body)
     if(!username || !email||!password||username===''||password===''){
        next(errorHandler(400,'All fields are required'));
     }
