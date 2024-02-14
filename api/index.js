05092b0f79a4ce855aad4496bc2fd1678dfc04de
Bikal69 <143072556+Bikal69@users.noreply.github.com>
2024-02-14 22:01:15 +0545

    add update user api route

--- api/index.js
+++ api/index.js
@@ -2,9 +2,11 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
-import authRoutes from './routes/auth.route.js'
+import authRoutes from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 const app=express()
 app.use(express.json());
+app.use(cookieParser())
 dotenv.config();
 mongoose.connect(process.env.MONGO_URI).then(()=>{
     console.log('successfully connected to db')
