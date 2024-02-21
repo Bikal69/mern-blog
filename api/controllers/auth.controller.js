c05a56d559cf37e483912a3641e5bd496f01f45c
Bikal69 <143072556+Bikal69@users.noreply.github.com>
2024-02-21 15:43:43 +0545

    add admin data in the backend and send it as a cookie in frontend

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -37,7 +37,7 @@ if(!validPassword){
   return  next(errorHandler(400,'Wrong credientials'));//we used wrong creditential msg instead of being specific to what is incorrect because we don't want to give hacker a hint which one is incorrect
 }
 const token=jwt.sign(
-    { id:validUser._id },process.env.JWT_SECRET
+    { id:validUser._id,isAdmin:validUser.isAsmin },process.env.JWT_SECRET
 );
 const {password:pass,...rest}=validUser._doc;//._doc gives the js object containing user data in javascript object format
 res.status(200).cookie('access_token',token,{httpOnly:true}).json(rest)
@@ -51,7 +51,7 @@ export const google=async(req,res,next)=>{
     try{
         const user=await User.findOne({email});
         if(user){
-            const token=jwt.sign({id:user._id},process.env.JWT_SECRET);
+            const token=jwt.sign({id:user._id,isAdmin:user.isAdmin},process.env.JWT_SECRET);
             const {password,...rest}=user._doc;
             res.status(200).cookie('access_token',token,{
                 httpOnly:true,
@@ -66,7 +66,7 @@ export const google=async(req,res,next)=>{
                profilePicture:googlePhotoUrl,
             });
             await newUser.save();
-            const token=jwt.sign({id:newUser._id},process.env.JWT_SECRET);
+            const token=jwt.sign({id:newUser._id,isAdmin:newUser.isAdmin},process.env.JWT_SECRET);
             const {password,...rest}=newUser._doc;
             res.status(200).cookie('access_token',token,{httpOnly:true}).json(rest);
         }
