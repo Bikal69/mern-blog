a20fa32c4464a6edb6cb1142706ba01a7bfc4634
Bikal69 <143072556+Bikal69@users.noreply.github.com>
2024-02-25 13:54:39 +0545

    add create a post api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js';
+import postRoutes from './routes/post.route.js';
 import cookieParser from 'cookie-parser';
 const app=express()
 app.use(express.json());
@@ -14,6 +15,7 @@ mongoose.connect(process.env.MONGO_URI).then(()=>{
 
 app.use('/api/user',userRoutes);
 app.use('/api/auth',authRoutes);
+app.use('/api/post',postRoutes);
 //error handling middleware
 app.use((err,req,res,next)=>{
     const statusCode=err.statusCode||500;
