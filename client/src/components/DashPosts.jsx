b683bbad6fd0fc168103acdfe41bca0fa9227b02
Bikal69 <143072556+Bikal69@users.noreply.github.com>
2024-03-02 14:41:55 +0545

    add delete post functionlity to the dashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,14 +1,17 @@
-import { Table, TableHead,Spinner } from 'flowbite-react';
+import { Table, TableHead,Spinner, Button, Modal,Toast } from 'flowbite-react';
 import React, { useEffect, useState } from 'react'
+import { HiOutlineExclamationCircle,HiX } from 'react-icons/hi';
 import {useSelector} from 'react-redux'
 import { Link } from 'react-router-dom';
 
 export default function DashPosts() {
   const {currentUser}=useSelector((state)=>state.user);
+  const [showToast, setShowToast] = useState(false);
   const [userPosts,setUserPosts]=useState([]);
   const [showMore,setShowMore]=useState(true);
   const  [showMoreLoading,setShowMoreLoading]=useState(false);
-
+  const [showModal,setShowModal]=useState(false);
+const [PostIdToDelete,setPostIdToDelete]=useState('')
   useEffect(()=>{
     console.log('render')
 const fetchPosts=async()=>{
@@ -46,6 +49,24 @@ if(res.ok){
       
     }
   }
+  //delete post function
+  const handleDeletePost=async()=>{
+setShowModal(false);
+try{
+const res=await fetch(`/api/post/deletepost/${PostIdToDelete}/${currentUser._id}`,{
+  method:'DELETE',
+});
+const data=await res.json();
+if(!res.ok){
+console.log(data.message)
+}else{
+  setShowToast(true);
+  setUserPosts((prev)=>prev.filter((post)=>post._id!==PostIdToDelete));
+}
+}catch(error){
+  
+}
+  }
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
       {currentUser.isAdmin&&userPosts.length>0?(
@@ -79,7 +100,10 @@ if(res.ok){
                 {post.category}
               </Table.Cell>
               <Table.Cell>
-                <span className='font-medium text-red-500 cursor-pointer hover:underline'>Delete</span>
+                <span onClick={()=>{
+                    setShowModal(true);
+                    setPostIdToDelete(post._id);
+                }} className='font-medium text-red-500 cursor-pointer hover:underline'>Delete</span>
               </Table.Cell>
               <Table.Cell className='text-teal-500 hover:underline'><Link to={`/update-post/${post._id}`}>
                 <span>Edit</span>
@@ -98,7 +122,35 @@ if(res.ok){
         }
         </>
       ):(<p>You don't have any posts yet!</p>)}
+       <Modal show={showModal} onClose={()=>setShowModal(false)} popup size='md'>
+          <Modal.Header/>
+          <Modal.Body>
+            <div className="text-center">
+              <HiOutlineExclamationCircle className='h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto'/>
+              <h3 className='mb-5 text-lg text-gray-500 dark:text-gray-400'>Are you sure you want to delete this post?</h3>
+              <div className="flex justify-center gap-4">
+                <Button color='failure' onClick={handleDeletePost}>
+                  Yes I am sure
+                </Button>
+                <Button color='gray' onClick={()=>setShowModal(false)}>
+                  No,cancel
+                </Button>
+              </div>
+            </div>
+          </Modal.Body>
+          </Modal>
+          {/* tost code */}
+          {
+            showToast&&(
+              <Toast className='fixed top-0 right-0'>
+              <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-red-300 text-red-500 dark:bg-red-800 dark:text-red-200">
+                <HiX className="h-5 w-5" />
+              </div>
+              <div className="ml-3 text-sm font-normal">Post has been deleted.</div>
+              <Toast.Toggle onDismiss={()=>setShowToast(false)} />
+            </Toast>
+            )
+          }
     </div>
-   
-  )
+  );
 }
