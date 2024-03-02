441d46850b7960e1a081ec3dcc0b6162ad5749a7
Bikal69 <143072556+Bikal69@users.noreply.github.com>
2024-03-02 14:07:06 +0545

    add show more functionality to the post results of the dashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,4 +1,4 @@
-import { Table, TableHead } from 'flowbite-react';
+import { Table, TableHead,Spinner } from 'flowbite-react';
 import React, { useEffect, useState } from 'react'
 import {useSelector} from 'react-redux'
 import { Link } from 'react-router-dom';
@@ -6,15 +6,20 @@ import { Link } from 'react-router-dom';
 export default function DashPosts() {
   const {currentUser}=useSelector((state)=>state.user);
   const [userPosts,setUserPosts]=useState([]);
+  const [showMore,setShowMore]=useState(true);
+  const  [showMoreLoading,setShowMoreLoading]=useState(false);
 
   useEffect(()=>{
-    console.log(userPosts)
+    console.log('render')
 const fetchPosts=async()=>{
   try{
     const res=await fetch(`/api/post/getposts?userId=${currentUser._id}`)
     const data=await res.json()
 if(res.ok){
   setUserPosts(data.posts)
+  if(data.posts.length<9){
+    setShowMore(false);
+  }
 }
   }catch(error){
     console.log(error.message)
@@ -23,7 +28,24 @@ if(res.ok){
 if(currentUser.isAdmin){
   fetchPosts();
 }
-  },[currentUser._id])
+  },[currentUser._id]);
+  const handleShowMore=async()=>{
+    const startIndex=userPosts.length;
+    setShowMoreLoading(true)
+    try{
+const res=await fetch( `/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
+const data=await res.json();
+setShowMoreLoading(false)
+if(res.ok){
+  setUserPosts((prev)=>[...prev,...data.posts]);
+  if(data.posts.length<9){
+    setShowMore(false);
+  }
+}
+    }catch(error){
+      
+    }
+  }
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
       {currentUser.isAdmin&&userPosts.length>0?(
@@ -67,6 +89,13 @@ if(currentUser.isAdmin){
           </Table.Body>
         ))}
         </Table>
+        {
+          showMore&&(
+            <button onClick={handleShowMore} className='w-full text-teal-500 self-center text-sm py-7'>
+              {showMoreLoading?<Spinner size='md'/>:' Show More'}
+            </button>
+          )
+        }
         </>
       ):(<p>You don't have any posts yet!</p>)}
     </div>
