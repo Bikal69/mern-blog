40d47b8faf91231eddb9e4b5da5b75ae5597c446
Bikal69 <143072556+Bikal69@users.noreply.github.com>
2024-06-21 12:09:02 +0545

    added static express library

--- api/index.js
+++ api/index.js
@@ -22,7 +22,10 @@ app.use('/api/auth',authRoutes);
 app.use('/api/post',postRoutes);
 app.use('/api/comment',commentRoutes)
 console.log(__dirname)
-// app.use(express.static(path.join(__dirname,'/client/dist')));
+app.use(express.static(path.join(__dirname,'/client/dist')));
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','dist','index.html'))
+});
 //error handling middleware
 app.use((err,req,res,next)=>{
     const statusCode=err.statusCode||500;
