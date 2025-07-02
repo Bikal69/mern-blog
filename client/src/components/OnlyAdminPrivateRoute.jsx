a0c7c82d591a2f34293121e085838ad4667b8e42
Bikal69 <143072556+Bikal69@users.noreply.github.com>
2025-07-02 19:15:02 +0545

    bug fix

--- client/src/components/OnlyAdminPrivateRoute.jsx
+++ client/src/components/OnlyAdminPrivateRoute.jsx
@@ -3,6 +3,9 @@ import { useSelector } from 'react-redux'
 import { Outlet,Navigate } from 'react-router-dom'
 export default function OnlyAdminPrivateRoute() {
      const {currentUser}=useSelector((state)=>state.user)
+     if(!currentUser){
+       console.log('No admin ')
+     }f
   return (currentUser?.isAdmin?(<Outlet/>):(<Navigate to='/sign-in'/>)
   )
 }
